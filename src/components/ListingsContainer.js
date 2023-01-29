3b0177772e1650483fb15bd437e45b4401915656
lizdmukami <lizdmukami@gmail.com>
2023-01-29 05:52:53 +0300

    Display list of all items when page is rendered

--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,25 @@
-import React from "react";
-// import ListingCard from "./ListingCard";
+import { useEffect, useState } from "react";
+import ListingCard from "./ListingCard";
 
 function ListingsContainer() {
+  const [listings, setListings] = useState([])
+
+  useEffect(()=> {
+    fetch("http://localhost:6001/listings")
+    .then(res => res.json())
+    .then((data) => setListings(() => data))
+  }, []);
+
+  const listLoop = listings.map((listing)=> {
+    return(
+      <ListingCard key={listing.id} id={listing.id} description={listing.description} image={listing.image} location={listing.location} />
+    )
+  })
+
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {listLoop}
       </ul>
     </main>
   );
